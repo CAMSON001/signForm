1334729016ab5f83bb1f1e4c2c8936ff4b5a0cff
Camara Vamoro <cvamorocamson@gmail.com>
2023-08-11 16:47:56 +0100

    lastTouch

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ let signUpCaller = document.querySelector(".signUpBtn");
 let signInCaller = document.querySelector(".signInBtn"); 
 let signInForm = document.querySelector(".signIn"); 
 let signUpForm = document.querySelector(".signUp"); 
+let OurSection = document.querySelector(".OurSection");
 
 
 function signUpfct(){
     signUpForm.classList.add('active'); 
     signInForm.classList.add('active')
+    OurSection.classList.add('active')
 }
 
 
 function signInfct(){
     signUpForm.classList.remove('active'); 
     signInForm.classList.remove('active')
+    OurSection.classList.remove('active')
 }
 
 
