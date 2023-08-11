530afd5d3d29d36f9aa6ce6e1440de5467ea750d
Camara Vamoro <cvamorocamson@gmail.com>
2023-08-11 16:13:27 +0100

    endOfJs

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-signUpBtn = document.querySelector(".signUpBtn");
+let signUpCaller = document.querySelector(".signUpBtn");
+let signInCaller = document.querySelector(".signInBtn"); 
+let signInForm = document.querySelector(".signIn"); 
+let signUpForm = document.querySelector(".signUp"); 
+
+
+function signUpfct(){
+    signUpForm.classList.add('active'); 
+    signInForm.classList.add('active')
+}
+
+
+function signInfct(){
+    signUpForm.classList.remove('active'); 
+    signInForm.classList.remove('active')
+}
+
+
+
+
+
